babbb377ceeb72cf5974051c41063227bbc40305
joyrahman <joyrahman@gmail.com>
2019-03-28 12:56:08 -0500

    yaml file added

--- clusterConfig.py
+++ clusterConfig.py
@@ -63,7 +63,7 @@ def load_yaml_job_spec(cntCompletions=10,cntParallelism=2,zone="red",jobType="me
     if jobType == "memory" or "":
         with open('stream.yaml','r') as f:
             body = yaml.load(f) 
-    if body not None:    
+    if body != None:    
         body['spec']['parallelism'] = cntParallelism
         body['spec']['completions'] = cntCompletions
         body['spec']['parallelism'] = cntParallelism
