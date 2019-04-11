d85dcc003f734141ccaa702d5b5184b1f8600b78
joy <joyrahman@gmail.com>
2019-04-10 23:56:52 -0500

    run.sh updated

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-python testRun.py -f ./inputTest.txt -pName prometheus-prometheus-prometheus-oper-prometheus-0 -k8url 10.52.0.73:31407 -locustF /home/cloudsys/joy-locust/locust-k8-loadtest/robot-shop.py
+export web_url=`kubectl get nodes -o wide | grep kb-m11  | awk '{print $6}'`
+export web_port=`kubectl get services  | grep web | awk '{print $5}' | cut -d ":" -f2 | cut -d "/" -f1`
+python testRun.py -f ./inputTest.txt -pName prometheus-prometheus-prometheus-oper-prometheus-0 -k8url $web_url:$web_port -locustF /home/cloudsys/joy-locust/locust-k8-loadtest/robot-shop.py
